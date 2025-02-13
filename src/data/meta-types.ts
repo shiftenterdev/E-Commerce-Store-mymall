475ae1ed13a6ffebf2c13dcd77c3b01adb1662b2
Iftakharul Alam <iftakharul.ca@gmail.com>
2025-02-13 08:45:17 +0100

    wip: og info added

--- src/data/meta-types.ts
+++ src/data/meta-types.ts
@@ -1,7 +1,10 @@
 export interface meta {
 	title: string
 	description: string
+	ogTitle?: string
+	ogDescription?: string
 	image?: string
+	ogImage?: string
 }
 
 export type metaContainer = {
--- src/data/meta.ts
+++ src/data/meta.ts
@@ -6,40 +6,62 @@ import { meta, metaContainer } from './meta-types.ts'
 
 export const landingPageMeta: meta = {
 	title: 'Mymall.nl | Mechanical Keyboard Store',
+	ogTitle: 'Mymall.nl | Mechanical Keyboard Store',
 	description:
 		'We sell custom mechanical keyboards, keycaps and deskmats for your gaming and office setup, right in the heart of Berlin.',
+	ogDescription:
+		'We sell custom mechanical keyboards, keycaps and deskmats for your gaming and office setup, right in the heart of Berlin.',
 	image: bannerImage,
+	ogImage: bannerImage,
 }
 
 export const categoryPageMeta: metaContainer = {
 	keyboards: {
 		title: 'All Mechanical Keyboards | Mymall',
+		ogTitle: 'All Mechanical Keyboards | Mymall',
 		description:
 			'Explore our selection of high quality mechanical keyboards that will elevate your typing experience.',
+		ogDescription:
+			'Explore our selection of high quality mechanical keyboards that will elevate your typing experience.',
 		image: keyboardsImage,
+		ogImage: keyboardsImage,
 	},
 	keycaps: {
 		title: 'All Custom Keycaps | Mymall',
+		ogTitle: 'All Custom Keycaps | Mymall',
 		description:
 			'Enjoy typing with style and upgrade your custom mechanical keyboard with our elegant keycaps',
+		ogDescription:
+			'Enjoy typing with style and upgrade your custom mechanical keyboard with our elegant keycaps',
 		image: keycapsImage,
+		ogImage: keycapsImage,
 	},
 	deskmats: {
 		title: 'All Deskmats | Mymall',
+		ogTitle: 'All Deskmats | Mymall',
 		description:
 			'Make yourself comfortable in your work or gaming setup with one of our KIIIBS deskmats. Soft and stylish!',
+		ogDescription:
+			'Make yourself comfortable in your work or gaming setup with one of our KIIIBS deskmats. Soft and stylish!',
 		image: deskmatsImage,
+		ogImage: deskmatsImage,
 	},
 }
 
 export const checkoutPageMeta: meta = {
 	title: 'Checkout',
+	ogTitle: 'Checkout',
 	description: 'Complete your purchase.',
+	ogDescription: 'Complete your purchase.',
 	image: bannerImage,
+	ogImage: bannerImage,
 }
 
 export const fofPageMeta: meta = {
 	title: '404: Page not found.',
+	ogTitle: '404: Page not found.',
 	description: '404: Page not found.',
+	ogDescription: '404: Page not found.',
 	image: bannerImage,
+	ogImage: bannerImage,
 }
