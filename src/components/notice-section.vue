1ad7c94590a2b4ce743ac4490df9ba6233fdf179
Iftakharul Alam <iftakharul.ca@gmail.com>
2025-02-18 10:32:43 +0100

    wip

--- src/components/notice-section.vue
+++ src/components/notice-section.vue
@@ -2,13 +2,14 @@
 	<div class="notice">
 		<p>
 			Deze website is in ontwikkeling. Sommige functies zijn nog niet beschikbaar.
+			contact <ins><a href="mailto:info@mymall.nl">info@mymall.nl</a></ins> voor meer informatie.
 		</p>
 	</div>
 </template>
 
 <style scoped>
 .notice {
-    background-color: red;
+    background-color: #f0f0f0;
     padding: 10px;
     border-radius: 0;
     text-align: center;
